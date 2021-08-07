8e008a1c0c604d4269616681ede086b2f7c02e6f
LucioMart <luchoweb15@gmail.com>
2021-08-07 13:36:19 -0300

    modularizando el toTonsand.

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,14 +5,15 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = require('../utils/toThosand')
 
 const controller = {
 
 	index: (req, res) => {
 		// res.send(products)
 		return res.render('index' , {
-			products
+			products,
+			toThousand
 		})
 	},
 	search: (req, res) => {
