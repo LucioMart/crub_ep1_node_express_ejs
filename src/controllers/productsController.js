d03666cf40e4677eb0cf678686332de3f38022cf
LucioMart <luchoweb15@gmail.com>
2021-08-15 11:02:47 -0300

    Desarrollada la logica para product create. Realizando product edit.

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -31,17 +31,34 @@ const controller = {
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		return res.render('product-create-form')
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+		const {name, price, discount, category, description} = req.body
+		let product = {
+			id : products[products.length - 1].id + 1,
+			name, 
+			price: +price,
+			discount : +discount,
+			category,
+			description,
+			image: 'default-image.png'
+		}
+		products.push(product)
+		// res.send(products)
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2), 'utf-8')
+		res.redirect('/products')
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		let product = products.find(product => product.id === +req.params.id)
+
+		return res.render('product-edit-form', {
+			product
+		})
 	},
 	// Update - Method to update
 	update: (req, res) => {
